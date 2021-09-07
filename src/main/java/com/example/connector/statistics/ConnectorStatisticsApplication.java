4bae6fcb85b28266e48883a20c1a3f5b96832be3
Barry <1172807506@qq.com>
2021-09-07 11:16:55 +0800

    'init'

---
+++ src/main/java/com/example/connector/statistics/ConnectorStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.connector.statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectorStatisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectorStatisticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/connector/statistics/ConnectorStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.connector.statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectorStatisticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
